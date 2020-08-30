a88db39194f8459ebdac9f0887eb597143ebf584
Ben Darrah <hooflessmoofless@gmail.com>
2020-08-31 11:29:39 +1200

    Implemented dialog boxes, separated out turn methods

--- SWEN225/src/Input.java
+++ SWEN225/src/Input.java
@@ -1,10 +1,13 @@
 
-import java.util.List
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.util.List;
+import java.awt.event.*;
+import javax.swing.*;
 
-public class Input implements KeyListener {
+public class Input implements KeyListener, ActionListener{
 
+	private static JTextField textField;
+	private static JLabel jLabel1;
+	
     //Allows access to the Board.
     Board myBoard;
 
@@ -12,14 +15,14 @@ public class Input implements KeyListener {
         this.myBoard = myBoard;
     }
 
-    public static String askID() {
-		// TODO Auto-generated method stub
-		return null;
+    public static String askID(int i) {
+    	String text = JOptionPane.showInputDialog("Player " + i + " please enter your name");    	
+    	return text;
 	}
 
 	public static String askPlayer() {
-		// TODO Auto-generated method stub
-		return null;
+		String text = JOptionPane.showInputDialog("DEBUG PLACEHOLDER: Enter Character name in all caps");
+		return text;
 	}
 
 	public static List<String> getAccusation() {
@@ -40,5 +43,11 @@ public class Input implements KeyListener {
     }
 	public void keyReleased(KeyEvent keyEvent){}
 	public void keyTyped(KeyEvent keyEvent){}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
